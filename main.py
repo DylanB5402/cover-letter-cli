acc2fdbfd4ea995ec7070fd6ad8e8d234a24b819
DylanB5402 <d.barva5402@gmail.com>
2022-09-17 14:55:40 -0700

    Use Typer to make a CLI

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 from jinja2 import Environment, FileSystemLoader
 import os
 import pdfkit
+import typer
 
-env = Environment(loader = FileSystemLoader(os.getcwd()))
-template = env.get_template('cover_letter_template.html')
+def generate_cover_letter(company, position):
+    env = Environment(loader = FileSystemLoader(os.getcwd()))
+    template = env.get_template('cover_letter_template.html')
 
-with open('cover_letter.html', 'w') as fh:
-    fh.write(template.render(
-        company = 'Appfolio',
-        position = 'Software Engineer Intern'
-    ))
-fh.close()
+    with open('out/cover_letter.html', 'w') as fh:
+        fh.write(template.render(
+            company = company,
+            position = position
+        ))
+    fh.close()
 
-options = {
-    'margin-top': '0.75in',
-    'margin-right': '0.75in',
-    'margin-bottom': '0.75in',
-    'margin-left': '0.75in',
-    'encoding': "UTF-8",
-}
+    options = {
+        'margin-top': '1in',
+        'margin-right': '1in',
+        'margin-bottom': '1in',
+        'margin-left': '1in',
+        'encoding': "UTF-8",
+    }
 
-pdfkit.from_file('cover_letter.html', 'cover_letter.pdf', options=options)
+    pdfkit.from_file('out/cover_letter.html', f'out/cover_letter_{company}.pdf', options=options)
+
+def main(company: str, position: str):
+    generate_cover_letter(company, position)
+
+if __name__ == "__main__":
+    typer.run(main)
